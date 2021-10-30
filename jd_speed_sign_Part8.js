3d1c8a68a2ace1f8e4727a3dbe6dbad5490f6742
Kylin <q657019212@gmail.com>
2021-10-30 21:10:56 +0800

    Update jd_speed_sign_Part8.js

--- jd_speed_sign_Part8.js
+++ jd_speed_sign_Part8.js
@@ -2,7 +2,7 @@
 cron "0 7 * * *" jd_speed_sign_Part8.js, tag:京东极速版任务8
 */
  //详细说明参考 https://github.com/ccwav/QLScript2.
-const $ = new Env('京东极速版任务1');
+const $ = new Env('京东极速版任务8');
 
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
